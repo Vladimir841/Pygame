e3103b940ae2fd246948f8f8f8cb4ed8432a9c10
Vladimir841 <Sam9144@yandex.ru>
2022-05-14 03:25:46 +0400

    Practic

---
+++ hangman.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+print('\nWelcome to Hangman game by DataFair\n')
+name = input('Enetr your name: ')
+print(f'Hello {name} ! Best of Luck!')
+time.sleep(2)
+print("The game is about to start!\n Let's play Hangman!")
+time.sleep(3)
